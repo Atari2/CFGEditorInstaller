6695e4eedbefb3e27a330533a1f2269f51b19b5a
Alessio <alex.robassomero@gmail.com>
2021-05-18 19:39:20 +0200

    Removed submodule

--- build_script.ps1
+++ build_script.ps1
@@ -1,6 +1,6 @@
 # we build CFGEditorPlusPlus first
 Set-Location CFGEditorPlusPlus
-Start-Process build_script.bat
+Start-Process -Wait -NoNewWindow .\build_script.bat
 py .\zip_script.py
 Set-Location ..
 Copy CFGEditorPlusPlus\CFGEditor.zip Resources\CFGEditor.zip
