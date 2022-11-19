ab196c1b25ed8a69eb18f7a7778beb9f4bdaddb2
Alessio Rosiello <alex.robassomero@gmail.com>
2022-11-19 13:30:37 +0100

    Reuse the already existing script.

--- build_script.bat
+++ build_script.bat
@@ -1,9 +1,5 @@
 cd CFGEditorPlusPlus
-call "C:\Program Files\Microsoft Visual Studio\2012\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake.exe -S . -B build -GNinja "-DCMAKE_BUILD_TYPE:String=Release" "-DCMAKE_PREFIX_PATH:STRING=C:/Qt/6.4.0/msvc2019_64" "-DCMAKE_C_COMPILER:STRING=cl.exe" "-DCMAKE_CXX_COMPILER:STRING=cl.exe"
-cd build
-cmake.exe --build . --target all
-cd ..
+call build_script.bat
 py .\zip_script.py
 cd ..
 copy CFGEditorPlusPlus\CFGEditor.zip Properties\CFGEditor.zip
